d6418e92729b2d61168bb890c71744610146dacf
MdShakibSarker <sijumd947@gmail.com>
2026-06-15 23:57:01 +0600

    add new

--- test.dart
+++ test.dart
@@ -1,4 +1,35 @@
 void main() {
-  print("siju da ajk ki ganja hobe");
-  print("siju");
+  //print("siju da ajk ki ganja hobe");
+  //print("siju")
+  String Number = "500";
+  double number = 42.55;
+  String letter = " Shakib ";
+  bool right = true;
+  var Name = " shakib ";
+  int value = 100;
+  double mark = 99.99;
+  int parseNumber = int.parse(Number);
+  print(Name);
+  print(mark);
+  print(letter);
+  print(right);
+  print(parseNumber);
+
+  int NumberOne = 500;
+  int NumberTwo = 400;
+
+  int add = NumberOne + NumberTwo;
+
+  print(add);
+  double divide = NumberOne / NumberTwo;
+  print(divide);
+  int multiply = NumberTwo * NumberOne;
+  print(multiply);
+  int minus = NumberOne - NumberTwo;
+  print(minus);
+
+  bool isfun = true;
+  bool isfunDart = false;
+
+  print(isfun && !isfunDart);
 }
