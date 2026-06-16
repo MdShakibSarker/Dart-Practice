ebd3c1b8eed25406e6cdca99525751a7f284879c
MdShakibSarker <sijumd947@gmail.com>
2026-06-16 22:24:25 +0600

    add new

--- siju.dart
+++ siju.dart
@@ -1,1 +1,9 @@
+void main() {
+  int a = 15, b = 17;
 
+  print("Addition ${a + b}");
+  print("Substraction ${a - b}");
+  print("Multiplication ${a * b}");
+  print("Division ${a / b}");
+  print("Remainder ${a % b}");
+}
